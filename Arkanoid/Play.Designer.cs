930c241200307a9eb8581cbb6f6ecb628068d2b3
FernandoCarcamo <00084219@uca.edu.sv>
2020-06-09 00:09:38 -0600

    Commit: FormsCreation

---
+++ Arkanoid/Play.Designer.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+
+namespace Arkanoid
+{
+    partial class Play
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Play));
+            this.SuspendLayout();
+            // 
+            // Play
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1173, 666);
+            this.Name = "Play";
+            this.Text = "Play";
+            this.Resize += new System.EventHandler(this.Play_Resize);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
